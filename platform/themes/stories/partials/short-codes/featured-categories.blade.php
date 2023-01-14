39290714c225d74d480ceda1c67795b531dc2c74
mavanhung <29092461+mavanhung@users.noreply.github.com>
2023-01-14 18:15:18 +0700

    .

--- platform/themes/stories/partials/short-codes/featured-categories.blade.php
+++ platform/themes/stories/partials/short-codes/featured-categories.blade.php
@@ -1,6 +1,6 @@
 <div class="site-bottom pt-50 pb-50">
     <div class="container">
-        <div class="sidebar-widget widget-latest-posts mb-30 mt-20 wow fadeInUp animated">
+        <div class="carausel-3-columns-wrapper sidebar-widget widget-latest-posts mb-30 mt-20 wow fadeInUp animated">
             <div class="widget-header-2 position-relative mb-30">
                 <h5 class="mt-5 mb-30">{!! clean($title) !!}</h5>
             </div>
