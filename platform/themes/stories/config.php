85b04a04fb83c2fca0e5f889e408587b3319470a
mavanhung <29092461+mavanhung@users.noreply.github.com>
2023-02-15 00:13:15 +0700

    .

--- platform/themes/stories/config.php
+++ platform/themes/stories/config.php
@@ -83,8 +83,8 @@ return [
             $theme->asset()->container('footer')->usePath()->add('perfect-scrollbar', 'js/vendor/perfect-scrollbar.js');
             $theme->asset()->container('footer')->usePath()->add('waypoints', 'js/vendor/jquery.waypoints.min.js');
             $theme->asset()->container('footer')->usePath()->add('jquery.theia.sticky', 'js/vendor/jquery.theia.sticky.js');
-            $theme->asset()->container('footer')->add('fancybox-js', 'https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js', [], ['async']);
-            $theme->asset()->container('footer')->add('zalo-js', 'https://sp.zalo.me/plugins/sdk.js', [], ['async']);
+            // $theme->asset()->container('footer')->add('fancybox-js', 'https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js', [], ['async']);
+            // $theme->asset()->container('footer')->add('zalo-js', 'https://sp.zalo.me/plugins/sdk.js', [], ['async']);
 
             $theme->asset()->container('footer')->usePath()->add('comment', 'js/comment.js', ['jquery'], [], $version);
             $theme->asset()->container('footer')->usePath()->add('main', 'js/main.js', ['jquery'], [], $version);
