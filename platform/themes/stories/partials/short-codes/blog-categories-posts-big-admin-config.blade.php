5df72b29ec0dd6a166e8bdeb4755cd3b23625cf4
mavanhung <29092461+mavanhung@users.noreply.github.com>
2023-02-25 09:49:17 +0700

    no message

--- platform/themes/stories/partials/short-codes/blog-categories-posts-big-admin-config.blade.php
+++ platform/themes/stories/partials/short-codes/blog-categories-posts-big-admin-config.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group">
     <label class="control-label">{{ __('Select a category') }}</label>
-    <select name="category_id" class="form-control" data-shortcode-attribute="category_id">
+    <select name="category_id_1" class="form-control" data-shortcode-attribute="category_id_1">
         @foreach($categories as $category)
             <option value="{{ $category->id }}">{{ $category->name }}</option>
         @endforeach
