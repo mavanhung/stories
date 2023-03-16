62410fb4f1cfe9fbd7938812ed1417aa09518868
mavanhung <29092461+mavanhung@users.noreply.github.com>
2023-03-16 18:31:50 +0700

    .

--- app/Console/Commands/crawlers.php
+++ app/Console/Commands/crawlers.php
@@ -41,25 +41,25 @@ class crawlers extends Command
      */
     public function handle()
     {
-        $posts = Post::where('website', 'tuvanmuasam.com')->select('id', 'content')->get();
-        $count = 0;
-        $ids = [];
-        foreach ($posts as $post) {
-            $content = $post->content;
-            $check = strpos($content, 'Tư vấn mua sắm');
-            if(!($check === false)){
-                $ids[] = $post->id;
-                $count++;
-                $search = ['Tư Vấn Mua Sắm', 'Tư vấn mua sắm'];
-                $replace = ['XoaiChua', 'XoaiChua'];
-                $result = str_replace($search, $replace, $content);
-                $post->update([
-                    'content' => $result
-                ]);
-            }
-        }
-        $this->sendNotificationTelegram(implode(', ', $ids));
-        dump($count, $ids);
+        // $posts = Post::where('website', 'tuvanmuasam.com')->select('id', 'content')->get();
+        // $count = 0;
+        // $ids = [];
+        // foreach ($posts as $post) {
+        //     $content = $post->content;
+        //     $check = strpos($content, 'Tư vấn mua sắm');
+        //     if(!($check === false)){
+        //         $ids[] = $post->id;
+        //         $count++;
+        //         $search = ['Tư Vấn Mua Sắm', 'Tư vấn mua sắm'];
+        //         $replace = ['XoaiChua', 'XoaiChua'];
+        //         $result = str_replace($search, $replace, $content);
+        //         $post->update([
+        //             'content' => $result
+        //         ]);
+        //     }
+        // }
+        // $this->sendNotificationTelegram(implode(', ', $ids));
+        // dump($count, $ids);
 
         // https://phongreviews.com/sua-rua-mat-sk-ii/    click
         // https://phongreviews.com/son-espoir/   rutgon
@@ -92,60 +92,60 @@ class crawlers extends Command
 
 
 
-        // $website = $this->argument('website');
-        // if($website == 'phongreviews'){
-        //     $this->crawlersPhongReviews();
-        // }
-        // if($website == 'trustreview'){
-        //     $this->crawlersTrustReview();
-        // }
-        // if($website == 'tuvanmuasam'){
-        //     // https://tuvanmuasam.com/co-rua-binh-sua
-        //     // https://tuvanmuasam.com/gang-tay-tap-gym
-        //     // https://tuvanmuasam.com/kem-tri-tham-mong  //có chứa thẻ a khác
-        //     // https://tuvanmuasam.com/dau-goi-phu-bac //có link khác
-        //     // https://tuvanmuasam.com/chuot-bay-tot-nhat
-        //     // url: https://tuvanmuasam.com/usb-wifi
-        //     // https://tuvanmuasam.com/tong-do-cat-toc-cho-be //sai url
+        $website = $this->argument('website');
+        if($website == 'phongreviews'){
+            $this->crawlersPhongReviews();
+        }
+        if($website == 'trustreview'){
+            $this->crawlersTrustReview();
+        }
+        if($website == 'tuvanmuasam'){
+            // https://tuvanmuasam.com/co-rua-binh-sua
+            // https://tuvanmuasam.com/gang-tay-tap-gym
+            // https://tuvanmuasam.com/kem-tri-tham-mong  //có chứa thẻ a khác
+            // https://tuvanmuasam.com/dau-goi-phu-bac //có link khác
+            // https://tuvanmuasam.com/chuot-bay-tot-nhat
+            // url: https://tuvanmuasam.com/usb-wifi
+            // https://tuvanmuasam.com/tong-do-cat-toc-cho-be //sai url
 
-        //     //////////// Những link có chứa shorten.asia ///////////
-        //     // https://tuvanmuasam.com/may-rua-mat
-        //     // https://tuvanmuasam.com/collagen-nhat-tot-nhat
-        //     // https://tuvanmuasam.com/coc-nguyet-san
-        //     // https://tuvanmuasam.com/dai-nit-bung
-        //     // https://tuvanmuasam.com/tinh-dau-toi
-        //     // https://tuvanmuasam.com/bot-can-tay-giam-can
-        //     // https://tuvanmuasam.com/dau-goi-phu-bac
-        //     // https://tuvanmuasam.com/may-massage-chan
-        //     // https://tuvanmuasam.com/noi-chien-hoi-nuoc
-        //     // https://tuvanmuasam.com/rong-bien-ngon-nhat
-        //     // https://tuvanmuasam.com/may-rua-mat-halio
-        //     // https://tuvanmuasam.com/khau-trang-wakamono
-        //     // https://tuvanmuasam.com/review-rong-nho-yukibudo
-        //     // https://tuvanmuasam.com/dau-goi-phu-bac-sin-hair
-        //     // https://tuvanmuasam.com/dau-goi-dego-pharma
-        //     // https://tuvanmuasam.com/noi-chien-khong-dau-lotte
-        //     // https://tuvanmuasam.com/rong-nho-sabudo
-        //     // https://tuvanmuasam.com/coc-nguyet-san-ovacup
-        //     // https://tuvanmuasam.com/coc-nguyet-san-beucup
-        //     // https://tuvanmuasam.com/tien-dinh-khang
-        //     // https://tuvanmuasam.com/may-tam-nuoc-iris-care
-        //     // https://tuvanmuasam.com/sua-non-colomi
-        //     // https://tuvanmuasam.com/com-tri-nao-gbrain
+            //////////// Những link có chứa shorten.asia ///////////
+            // https://tuvanmuasam.com/may-rua-mat
+            // https://tuvanmuasam.com/collagen-nhat-tot-nhat
+            // https://tuvanmuasam.com/coc-nguyet-san
+            // https://tuvanmuasam.com/dai-nit-bung
+            // https://tuvanmuasam.com/tinh-dau-toi
+            // https://tuvanmuasam.com/bot-can-tay-giam-can
+            // https://tuvanmuasam.com/dau-goi-phu-bac
+            // https://tuvanmuasam.com/may-massage-chan
+            // https://tuvanmuasam.com/noi-chien-hoi-nuoc
+            // https://tuvanmuasam.com/rong-bien-ngon-nhat
+            // https://tuvanmuasam.com/may-rua-mat-halio
+            // https://tuvanmuasam.com/khau-trang-wakamono
+            // https://tuvanmuasam.com/review-rong-nho-yukibudo
+            // https://tuvanmuasam.com/dau-goi-phu-bac-sin-hair
+            // https://tuvanmuasam.com/dau-goi-dego-pharma
+            // https://tuvanmuasam.com/noi-chien-khong-dau-lotte
+            // https://tuvanmuasam.com/rong-nho-sabudo
+            // https://tuvanmuasam.com/coc-nguyet-san-ovacup
+            // https://tuvanmuasam.com/coc-nguyet-san-beucup
+            // https://tuvanmuasam.com/tien-dinh-khang
+            // https://tuvanmuasam.com/may-tam-nuoc-iris-care
+            // https://tuvanmuasam.com/sua-non-colomi
+            // https://tuvanmuasam.com/com-tri-nao-gbrain
 
-        //     // https://tuvanmuasam.com/chuot-bay-tot-nhat //có link google search
+            // https://tuvanmuasam.com/chuot-bay-tot-nhat //có link google search
 
-        //     //https://tuvanmuasam.com/kem-chong-nang-han-quoc //Lỗi hình ảnh
-        //     //https://tuvanmuasam.com/keo-dan-go //Lỗi hình ảnh
-        //     //https://tuvanmuasam.com/android-tv-box //Lỗi hình ảnh
-        //     //https://tuvanmuasam.com/quat-dieu-hoa-sunhouse //Lỗi hình ảnh
-        //     //https://tuvanmuasam.com/bang-ve-dien-tu //Lỗi hình ảnh
+            //https://tuvanmuasam.com/kem-chong-nang-han-quoc //Lỗi hình ảnh
+            //https://tuvanmuasam.com/keo-dan-go //Lỗi hình ảnh
+            //https://tuvanmuasam.com/android-tv-box //Lỗi hình ảnh
+            //https://tuvanmuasam.com/quat-dieu-hoa-sunhouse //Lỗi hình ảnh
+            //https://tuvanmuasam.com/bang-ve-dien-tu //Lỗi hình ảnh
 
-        //     $this->crawlersTuVanMuaSam();
-        // }
-        // if($website == 'removefolder'){
-        //     $this->removefolder();
-        // }
+            $this->crawlersTuVanMuaSam();
+        }
+        if($website == 'removefolder'){
+            $this->removefolder();
+        }
         // $this->crawlersPhongReviewsDetail(19, 'https://phongreviews.com/thuoc-nhuom-toc-han-quoc/', 'https://phongreviews.com/wp-content/uploads/2022/02/thuoc-nhuom-toc-han-quoc-0-1024x841.jpg');
         // $this->crawlersTrustReviewDetail(19, 'https://trustreview.vn/noi-chien-khong-dau-loai-nao-tot.html', 'https://trustreview.vn/wp-content/uploads/2020/11/noi-chien-khong-dau-loai-nao-tot-11-1-300x171.jpg');
         // $this->crawlersTuVanMuaSamDetail(19, 'https://tuvanmuasam.com/quay-cui-cho-be', 'https://tuvanmuasam.com/wp-content/uploads/2023/03/quay-cui-cho-be-300x240.jpg');
